832276562436971804ed4f174000c5cbaedef23d
rick077-oh <enriquemarques085@gmail.com>
2025-06-16 17:04:25 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,30 @@
+criaCartao(
+    'Programação',
+    "Qual é a cor do céu em um dia sem nuvens?"
+    "azul"
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
