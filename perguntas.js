6260d1ddd56ca201eddcd30499cf27faba3b65cb
rick077-oh <enriquemarques085@gmail.com>
2025-06-18 12:24:05 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,30 +1,30 @@
 criaCartao(
-    'Programação',
+    'Pergunta',
     "Qual é a cor do céu em um dia sem nuvens?",
     "azul"
 )
 criaCartao(
-    'Programação',
+    'Pergunta',
      "Quantos dedos temos nas mãos?",
      "Dez"
 )
 criaCartao(
-    'Programação',
+    'Pergunta',
       "Qual animal faz “miau”?",
     'Gato'
 )
 criaCartao(
-    'Programação',
+    'Pergunta',
     'Qual fruta é conhecida por ser amarela e ter muito potássio?',
     'Banana'
 )
 criaCartao(
-    'Programação',
+    'Pergunta',
     'O que usamos para escrever no caderno?',
     ' Lápis ou caneta'
 )
 criaCartao(
-    'Programação',
+    'Pergunta',
     'Em que continente fica o Brasil?',
     'América do Sul'
 )
